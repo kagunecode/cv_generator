a07a91f6be4816232e6b892f66f24ae9873a6a7a
kagunecode <cesar.acb98@gmail.com>
2023-11-30 12:10:18 -0500

    Added logic for content rendering on the component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import setState from "react";
 import Navbar from "./components/Navbar";
 import Mainform from "./components/Mainform";
+import sectionInfo from "./data/sectionInfo";
 
 function App() {
+	const [activeTab, setActiveTab] = useState(null);
+
+	function handleTabClick(tab) {
+		console.log(tab);
+		setActiveTab(tab);
+	}
+
 	return (
 		<div className="h-screen">
-			<Navbar></Navbar>
+			<Navbar
+				sectionInfo={sectionInfo}
+				handleTabClick={handleTabClick}
+			></Navbar>
 			<div className="w-[calc(100vw-3rem)] translate-x-[3rem] p-3">
-				<Mainform></Mainform>
+				<Mainform activeTab={activeTab}></Mainform>
 			</div>
 		</div>
 	);
