fe33c831f9bf6e8413caf14d83d80e86d94f315d
kagunecode <cesar.acb98@gmail.com>
2023-11-30 11:12:53 -0500

    Moved space declarations to the main App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Mainform from "./components/Mainform";
 
 function App() {
 	return (
 		<div className="h-screen">
 			<Navbar></Navbar>
+			<div className="w-[calc(100vw-3rem)] translate-x-[3rem] p-3">
+				<Mainform></Mainform>
+			</div>
 		</div>
 	);
 }
