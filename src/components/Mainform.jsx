bcbf7294da95fa93d6320398d47b74b5085b3856
kagunecode <cesar.acb98@gmail.com>
2023-11-30 11:13:17 -0500

    Added component for screen render

---
+++ src/components/Mainform.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Mainform() {
+	return (
+		<div>
+			<h1 className="text-4xl font-semibold">Welcome To CVG</h1>
+			<p className="font-light">
+				Welcome to the CV generator. Here, you will see some instructions on how
+				to use this tool.
+			</p>
+		</div>
+	);
+}
+
+export default Mainform;
