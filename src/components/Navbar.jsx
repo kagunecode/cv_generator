48b1741920eae29db8a97fc3dcd018c107371e82
kagunecode <cesar.acb98@gmail.com>
2023-11-30 11:13:39 -0500

    Added a sectionInfo object for state control

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,30 @@
 import "../styles/styles.css";
+import sectionInfo from "../data/sectionInfo";
 
 function Items(props) {
 	const itemList = props.navbarTabs.map((tab) => {
 		return (
 			<li
 				className="px-3 text-slate-400 font-semibold hover:cursor-default duration-200 hover:bg-slate-700 rounded-3xl hover:text-white hover:rounded-none"
-				key={tab}
+				key={tab.tabName}
 			>
-				{tab}
+				{tab.tabName}
 			</li>
 		);
 	});
-	return <ul className="flex">{itemList}</ul>;
+	return <ul className="flex">{itemList.reverse()}</ul>;
 }
 
 function Navbar() {
-	const navbarTabs = [
-		"General",
-		"Experience",
-		"Education",
-		"Skills",
-		"Other",
-		"Customize",
-	];
 	return (
 		<div
-			className="w-[100vh] fixed flex items-center justify-end py-2 px-2"
+			className="w-[100vh] h-12 fixed flex items-center justify-end py-2 px-2 border"
 			style={{
 				transform: "rotate(-90deg) translate(-100%, 0)",
 				transformOrigin: "left top",
 			}}
 		>
-			<Items navbarTabs={navbarTabs.reverse()}></Items>
+			<Items navbarTabs={sectionInfo.reverse()}></Items>
 			<h1 className="text-3xl font-bold rotate-90 ml-2 px-5 hover:rotate-180 duration-200 hover:cursor-default">
 				L
 			</h1>
---
+++ src/data/sectionInfo.js
@@ -0,0 +1,40 @@
+const sectionInfo = [
+	{
+		id: 1,
+		tabName: "General",
+		tabDescription: "All your basic information goes here.",
+		isActive: false,
+	},
+	{
+		id: 2,
+		tabName: "Experience",
+		tabDescription: "Add info about previous jobs or projects.",
+		isActive: false,
+	},
+	{
+		id: 3,
+		tabName: "Education",
+		tabDescription: "This will reflect your preparation for the role.",
+		isActive: false,
+	},
+	{
+		id: 4,
+		tabName: "Skills",
+		tabDescription: "Add everything you know and the level of knowledge.",
+		isActive: false,
+	},
+	{
+		id: 5,
+		tabName: "Other",
+		tabDescription: "Hobbies, languages and extra information.",
+		isActive: false,
+	},
+	{
+		id: 6,
+		tabName: "Customize",
+		tabDescription: "Make your CV stand out adding a little touch of you.",
+		isActive: false,
+	},
+];
+
+export default sectionInfo;
