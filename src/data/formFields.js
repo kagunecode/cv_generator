8c0da1096cf760ee24202445a1a13e9491cde46e
kagunecode <cesar.acb98@gmail.com>
2023-11-30 23:02:08 -0500

    Added data for input fields to render

---
+++ src/data/formFields.js
@@ -0,0 +1,14 @@
+const formFields = [
+	{
+		sectionName: "General",
+		fields: [
+			{ id: "fullname", fieldName: "Full Name", fieldType: "text" },
+			{ id: "title", fieldName: "Title", fieldType: "text" },
+			{ id: "email", fieldName: "Email", fieldType: "email" },
+			{ id: "phone", fieldName: "Phone", fieldType: "phone" },
+			{ id: "location", fieldName: "Location", fieldType: "text" },
+		],
+	},
+];
+
+export default formFields;
