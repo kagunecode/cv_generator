19d312b63dc49feb75398b4b0c8dae7f6533cd9b
kagunecode <cesar.acb98@gmail.com>
2023-11-30 23:01:41 -0500

    Added Form component for input render

---
+++ src/components/Form.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Fields(props) {
+	const fieldsContent = props.formFields.map((field) => {
+		return (
+			<div key={field.id} className="flex flex-col mb-6">
+				<label className="mb-1" htmlFor={field.id}>
+					{field.fieldName}
+				</label>
+				<input
+					className="w-[60%] h-9 bg-zinc-200 rounded px-1"
+					type={field.fieldType}
+					id={field.id}
+				/>
+			</div>
+		);
+	});
+	return <form>{fieldsContent}</form>;
+}
+
+function Form({ formFields }) {
+	return (
+		<>
+			<Fields formFields={formFields}></Fields>
+		</>
+	);
+}
+
+export default Form;
