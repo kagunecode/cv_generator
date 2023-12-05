39bb46053c37fd306e2792abe9672d0dfd58e27c
kagunecode <cesar.acb98@gmail.com>
2023-12-05 18:48:55 -0500

    Added custon tailwind animation

--- src/routes/ErrorPage.jsx
+++ src/routes/ErrorPage.jsx
@@ -4,9 +4,18 @@ function ErrorPage() {
 	const error = useRouteError();
 	console.log(error);
 	return (
-		<div className="flex flex-col h-screen items-center justify-center bg-gradient-to-r from-amber-200 to-yellow-500">
-			<h1 className="text-[8rem]">{error.status}</h1>
-			<p className="text-2xl text-slate-700">{error.statusText}</p>
+		<div className="flex flex-col h-screen items-center justify-center bg-gradient-to-tr from-emphasis-500 via-yellow-300 to-yellow-200">
+			<div className="relative">
+				<h1 className="text-[10rem] text-white font-semibold animate-errortext">
+					{error.status}
+				</h1>
+				<h1 className="absolute text-black text-[10rem] top-0 font-semibold">
+					{error.status}
+				</h1>
+			</div>
+			<p className="text-3xl font-semibold text-slate-700">
+				{error.statusText}
+			</p>
 		</div>
 	);
 }
