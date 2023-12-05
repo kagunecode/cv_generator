8b5411bad0d045a521a4d121ce9b0004166e2788
kagunecode <cesar.acb98@gmail.com>
2023-12-05 17:18:22 -0500

    Added react router to handle tab navigation

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Root from "./routes/Root";
+import General from "./routes/General";
+import ErrorPage from "./routes/ErrorPage";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Root />,
+		errorElement: <ErrorPage />,
+		children: [
+			{
+				errorElement: <ErrorPage />,
+				children: [
+					{
+						path: "/general",
+						element: <General />,
+					},
+				],
+			},
+		],
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<App></App>
+		<RouterProvider router={router} />
 	</React.StrictMode>
 );
