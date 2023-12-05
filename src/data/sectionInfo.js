bb14d9e3a0a8a51755cebb86fb8916520cde21ac
kagunecode <cesar.acb98@gmail.com>
2023-12-05 17:19:02 -0500

    Changed data for future use

--- src/data/sectionInfo.js
+++ src/data/sectionInfo.js
@@ -2,37 +2,37 @@ const sectionInfo = [
 	{
 		id: 1,
 		tabName: "General",
-		tabDescription: "All your basic information goes here.",
+		tabUrl: "general",
 		isActive: false,
 	},
 	{
 		id: 2,
 		tabName: "Experience",
-		tabDescription: "Add info about previous jobs or projects.",
+		tabUrl: "experience",
 		isActive: false,
 	},
 	{
 		id: 3,
 		tabName: "Education",
-		tabDescription: "This will reflect your preparation for the role.",
+		tabUrl: "education",
 		isActive: false,
 	},
 	{
 		id: 4,
 		tabName: "Skills",
-		tabDescription: "Add everything you know and the level of knowledge.",
+		tabUrl: "skills",
 		isActive: false,
 	},
 	{
 		id: 5,
 		tabName: "Other",
-		tabDescription: "Hobbies, languages and extra information.",
+		tabUrl: "other",
 		isActive: false,
 	},
 	{
 		id: 6,
 		tabName: "Customize",
-		tabDescription: "Make your CV stand out adding a little touch of you.",
+		tabUrl: "customize",
 		isActive: false,
 	},
 ];
