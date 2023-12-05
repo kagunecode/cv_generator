92cc6788e7b7186d07a5a16bc93557b9e34b9de9
kagunecode <cesar.acb98@gmail.com>
2023-12-05 18:48:46 -0500

    Added subtle animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,25 @@ export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {
+			animation: {
+				errortext: "errortext infinite 4s ease-in-out",
+			},
+			keyframes: {
+				errortext: {
+					"0%": {
+						transform: "translate(0rem, 0rem)",
+					},
+					"33%": {
+						transform: "translate(0.25rem, 0.25rem)",
+					},
+					"66%": {
+						transform: "translate(-0.25rem, -0.25rem)",
+					},
+					"100%": {
+						transform: "translate(0rem, 0rem)",
+					},
+				},
+			},
 			colors: {
 				emphasis: {
 					500: "#FFC107",
