cd7689c98366388529dfc2c236d3ced28b1ec67b
kagunecode <cesar.acb98@gmail.com>
2023-12-06 17:02:33 -0500

    Added transition animations

---
+++ src/routes/Experience.jsx
@@ -0,0 +1,20 @@
+import formFields from "../data/formFields";
+import Forms from "../components/Forms";
+import { AnimatedPage } from "../components/AnimatedPage";
+
+function Experience() {
+	const experienceForm = formFields.find(
+		(section) => section.sectionName === "Experience"
+	);
+	return (
+		<AnimatedPage>
+			<div>
+				<h1 className="text-5xl font-semibold">Experience</h1>
+				<p>Add previous jobs related to the job you're applying to</p>
+				<Forms formFields={experienceForm.fields}></Forms>
+			</div>
+		</AnimatedPage>
+	);
+}
+
+export default Experience;
--- src/routes/General.jsx
+++ src/routes/General.jsx
@@ -1,16 +1,19 @@
 import Forms from "../components/Forms";
 import formFields from "../data/formFields";
+import { AnimatedPage } from "../components/AnimatedPage";
 
 function General() {
 	const generalForm = formFields.find(
 		(section) => section.sectionName === "General"
 	);
 	return (
-		<div>
-			<h1 className="text-5xl font-semibold">General</h1>
-			<p>All your basic information goes here</p>
-			<Forms formFields={generalForm.fields}></Forms>
-		</div>
+		<AnimatedPage>
+			<div>
+				<h1 className="text-5xl font-semibold">General</h1>
+				<p>All your basic information goes here</p>
+				<Forms formFields={generalForm.fields}></Forms>
+			</div>
+		</AnimatedPage>
 	);
 }
 
