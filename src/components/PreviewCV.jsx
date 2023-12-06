69bd9a4dd4752b6a9dbe3f02588c24959b40de71
kagunecode <cesar.acb98@gmail.com>
2023-12-05 23:15:46 -0500

    Added preview for the CV, prop only

---
+++ src/components/PreviewCV.jsx
@@ -0,0 +1,12 @@
+function PreviewCV() {
+	return (
+		<div className="h-full bg-white">
+			<div className="bg-emphasis-500 h-30 p-5">
+				<h1 className="text-2xl font-bold">Cesar Cruz</h1>
+				<p className="font-light">Electronic Engineer</p>
+			</div>
+		</div>
+	);
+}
+
+export default PreviewCV;
