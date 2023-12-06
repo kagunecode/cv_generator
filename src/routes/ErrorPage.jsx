f57ccc38ae191e8dba6c3f60bae42f59a4dadea4
kagunecode <cesar.acb98@gmail.com>
2023-12-06 13:24:30 -0500

    Removed API calls for errors due to limitations

--- src/routes/ErrorPage.jsx
+++ src/routes/ErrorPage.jsx
@@ -1,21 +1,18 @@
-import { useRouteError } from "react-router-dom";
-
 function ErrorPage() {
-	const error = useRouteError();
 	return (
 		<div className="flex relative overflow-hidden flex-col h-screen items-center justify-center bg-gradient-to-tr from-emphasis-500 via-yellow-300 to-yellow-200">
 			<h1 className="absolute text-white text-[200rem] hover:cursor-default select-none font-medium drop-shadow-2xl">
-				{error.status}
+				404
 			</h1>
 			<div className="relative">
 				<h1 className="text-[20rem] text-white font-semibold animate-errortext select-none">
-					{error.status}
+					404
 				</h1>
 				<h1 className="absolute text-black text-[20rem] top-0 font-semibold">
-					{error.status}
+					404
 				</h1>
 			</div>
-			<p className="text-4xl font-light text-slate-700">{error.statusText}</p>
+			<p className="text-4xl font-light text-slate-700">Page not found</p>
 		</div>
 	);
 }
