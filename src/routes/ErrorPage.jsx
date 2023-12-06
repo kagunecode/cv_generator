c164818417626303e1210b5b1449538559b3f7a0
kagunecode <cesar.acb98@gmail.com>
2023-12-05 23:16:02 -0500

    Styled the error page better with correct error

--- src/routes/ErrorPage.jsx
+++ src/routes/ErrorPage.jsx
@@ -2,20 +2,20 @@ import { useRouteError } from "react-router-dom";
 
 function ErrorPage() {
 	const error = useRouteError();
-	console.log(error);
 	return (
-		<div className="flex flex-col h-screen items-center justify-center bg-gradient-to-tr from-emphasis-500 via-yellow-300 to-yellow-200">
+		<div className="flex relative overflow-hidden flex-col h-screen items-center justify-center bg-gradient-to-tr from-emphasis-500 via-yellow-300 to-yellow-200">
+			<h1 className="absolute text-white text-[200rem] hover:cursor-default select-none font-medium drop-shadow-2xl">
+				{error.status}
+			</h1>
 			<div className="relative">
-				<h1 className="text-[10rem] text-white font-semibold animate-errortext">
+				<h1 className="text-[20rem] text-white font-semibold animate-errortext select-none">
 					{error.status}
 				</h1>
-				<h1 className="absolute text-black text-[10rem] top-0 font-semibold">
+				<h1 className="absolute text-black text-[20rem] top-0 font-semibold">
 					{error.status}
 				</h1>
 			</div>
-			<p className="text-3xl font-semibold text-slate-700">
-				{error.statusText}
-			</p>
+			<p className="text-4xl font-light text-slate-700">{error.statusText}</p>
 		</div>
 	);
 }
