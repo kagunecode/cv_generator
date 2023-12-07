faf52b8174c81e50c849edf4faeb8c1f6af4b60e
kagunecode <cesar.acb98@gmail.com>
2023-12-07 11:54:27 -0500

    Added index

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import ErrorPage from "./routes/ErrorPage";
 import Layout from "./components/Layout";
 import General from "./routes/General";
 import Experience from "./routes/Experience";
+import Welcome from "./routes/Welcome";
 
 function App() {
 	const location = useLocation();
@@ -21,7 +22,7 @@ function App() {
 						path="/experience"
 						element={<Layout toRender={Experience()} />}
 					/>
-					<Route index element={<></>} />
+					<Route index element={<Layout toRender={Welcome()} />} />
 					<Route path="*" element={<ErrorPage />} />
 				</Routes>
 			</AnimatePresence>
