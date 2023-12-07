1644350acb1eb297fa21ad4032a60c2cd4d0dcb8
kagunecode <cesar.acb98@gmail.com>
2023-12-07 17:41:56 -0500

    Changed render logic to pass the data to the CV

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,11 @@
 import PreviewCV from "./PreviewCV";
 
-export default function Layout({ toRender }) {
+export default function Layout({ children, userData }) {
 	return (
 		<div className="grid grid-cols-3 w-[calc(100vw-3rem)] h-full translate-x-[3rem] p-3">
-			<div className="col-span-2">{toRender}</div>
+			<div className="col-span-2">{children}</div>
 			<div className="bg-slate-200 p-2 col-span-1">
-				<PreviewCV></PreviewCV>
+				<PreviewCV formData={userData}></PreviewCV>
 			</div>
 		</div>
 	);
