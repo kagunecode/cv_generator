89d0347936c77e712de14e5fb42496610df3d372
kagunecode <cesar.acb98@gmail.com>
2023-12-07 17:42:37 -0500

    Added data structure for the CV information

---
+++ src/data/cvData.js
@@ -0,0 +1,23 @@
+const cvData = {
+	generalInfo: [
+		{
+			fullname: "John Doe",
+			title: "Web Developer",
+			email: "jhondoe@gmail.com",
+			phone: "999999999",
+			location: "New York - USA",
+		},
+	],
+	experience: [
+		{
+			id: 1,
+			position: "Software Engineer",
+			company: "Tech Co",
+			startDate: "2020-01-01",
+			endDate: "2021-12-31",
+			description: "Worked on exciting projects...",
+		},
+	],
+};
+
+export default cvData;
