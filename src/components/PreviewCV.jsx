303b9b1b85dffd35a41294e1ec8812326c885f42
kagunecode <cesar.acb98@gmail.com>
2023-12-07 17:42:16 -0500

    Started rendering logic, components needed

--- src/components/PreviewCV.jsx
+++ src/components/PreviewCV.jsx
@@ -1,10 +1,17 @@
-function PreviewCV() {
+function PreviewCV({ formData }) {
 	return (
 		<div className="h-full bg-white">
 			<div className="bg-emphasis-500 h-30 p-5">
-				<h1 className="text-2xl font-bold">Cesar Cruz</h1>
-				<p className="font-light">Electronic Engineer</p>
+				<h1 className="text-2xl font-bold">
+					{formData.generalInfo[0].fullname}
+				</h1>
+				<p className="font-light">{formData.generalInfo[0].title}</p>
 			</div>
+			<p className="font-light">{formData.generalInfo[0].email}</p>
+			<p className="font-light">{formData.generalInfo[0].phone}</p>
+			<p className="font-light">{formData.generalInfo[0].location}</p>
+			<p className="font-light">{formData.experience[0].company}</p>
+			<p className="font-light">{formData.experience[0].position}</p>
 		</div>
 	);
 }
