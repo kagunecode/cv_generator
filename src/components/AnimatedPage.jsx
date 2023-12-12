55e893971eb283db4966528e60cd9f4686ca9af0
kagunecode <cesar.acb98@gmail.com>
2023-12-12 12:37:15 -0500

    Removed exit transition for errorpage

--- src/components/AnimatedPage.jsx
+++ src/components/AnimatedPage.jsx
@@ -25,13 +25,6 @@ const animateError = (OgComponent) => {
 		<>
 			<OgComponent />
 			<motion.div
-				className="slide-in"
-				initial={{ scaleX: 0 }}
-				animate={{ scaleX: 0 }}
-				exit={{ scaleX: 1 }}
-				transition={{ duration: 0.6, ease: [0.22, 1, 0.36, 1] }}
-			/>
-			<motion.div
 				className="slide-out"
 				initial={{ scaleX: 1 }}
 				animate={{ scaleX: 0 }}
