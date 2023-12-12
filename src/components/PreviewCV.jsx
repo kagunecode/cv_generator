00f6f477b713473cbe438787df604e86129f91a9
kagunecode <cesar.acb98@gmail.com>
2023-12-12 08:50:46 -0500

    Removed userData argument. Added Data context hook
    The DataContext provides all the existing data with updates on other functions

--- src/components/PreviewCV.jsx
+++ src/components/PreviewCV.jsx
@@ -1,17 +1,24 @@
-function PreviewCV({ formData }) {
+import { useDataContext } from "../contexts/DataContext";
+
+function PreviewCV() {
+	const { data } = useDataContext();
 	return (
 		<div className="h-full bg-white">
 			<div className="bg-emphasis-500 h-30 p-5">
-				<h1 className="text-2xl font-bold">
-					{formData.generalInfo[0].fullname}
-				</h1>
-				<p className="font-light">{formData.generalInfo[0].title}</p>
+				<h1 className="text-2xl font-bold">{data.generalInfo[0].fullname}</h1>
+				<p className="font-light">{data.generalInfo[0].title}</p>
 			</div>
-			<p className="font-light">{formData.generalInfo[0].email}</p>
-			<p className="font-light">{formData.generalInfo[0].phone}</p>
-			<p className="font-light">{formData.generalInfo[0].location}</p>
-			<p className="font-light">{formData.experience[0].company}</p>
-			<p className="font-light">{formData.experience[0].position}</p>
+			<p className="font-light">{data.generalInfo[0].email}</p>
+			<p className="font-light">{data.generalInfo[0].phone}</p>
+			<p className="font-light">{data.generalInfo[0].location}</p>
+			{data.experience.map((job) => {
+				return (
+					<>
+						<h1 className="font-bold">{job.company}</h1>
+						<p className="font-light">{job.position}</p>
+					</>
+				);
+			})}
 		</div>
 	);
 }
