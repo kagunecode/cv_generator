c73055360552bef5537d54b50d7b8b709fba6082
kagunecode <cesar.acb98@gmail.com>
2023-12-12 08:46:34 -0500

    Added DataContextProvider for data context

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,23 @@
+//React Imports
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+// Elements
 import App from "./App";
+
+// Libraries
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import ErrorPage from "./routes/ErrorPage";
+import DataContextProvider from "./contexts/DataContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<Router>
-			<Routes>
-				<Route path="/*" element={<App />} />
-				<Route path="*" element={<ErrorPage />} />
-			</Routes>
-		</Router>
+		<DataContextProvider>
+			<Router>
+				<Routes>
+					<Route path="/*" element={<App />} />
+				</Routes>
+			</Router>
+		</DataContextProvider>
 	</React.StrictMode>
 );
