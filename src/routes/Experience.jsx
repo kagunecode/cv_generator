fd16e8abe0f4c0cecbf937b9f65a8551ce24f666
kagunecode <cesar.acb98@gmail.com>
2023-12-11 21:04:51 -0500

    Added 'add job' function with useState

--- src/routes/Experience.jsx
+++ src/routes/Experience.jsx
@@ -1,23 +1,67 @@
 import formFields from "../data/formFields";
 import Forms from "../components/Forms";
 import { AnimatedPage } from "../components/AnimatedPage";
+import cvData from "../data/cvData";
+import { useState } from "react";
+
+const experienceForm = formFields.find(
+	(section) => section.sectionName === "Experience"
+);
+
+function Jobs({ userData, setFormData, index }) {
+	return (
+		<div>
+			<Forms
+				formFields={experienceForm.fields}
+				userData={userData}
+				setFormData={setFormData}
+				sectionName="experience"
+				index={index}
+				key={index}
+			></Forms>
+		</div>
+	);
+}
 
 function Experience({ userData, setFormData }) {
-	const experienceForm = formFields.find(
-		(section) => section.sectionName === "Experience"
-	);
+	const newData = {
+		id: 0,
+		position: "",
+		company: "",
+		startDate: "",
+		endDate: "",
+		description: "",
+	};
+	function handleAddJob() {
+		const newJob = { ...newData, id: userData.experience.length };
+		setFormData((prevData) => ({
+			...prevData,
+			experience: [...prevData.experience, newJob],
+		}));
+		console.log(userData.experience);
+	}
 	return (
 		<AnimatedPage renderNum={experienceForm.fields.length}>
-			<div>
-				<h1 className="text-5xl font-semibold">Experience</h1>
-				<p>Add previous jobs related to the job you're applying to</p>
-				<Forms
-					formFields={experienceForm.fields}
-					userData={userData}
-					setFormData={setFormData}
-					sectionName="experience"
-					index={0}
-				></Forms>
+			<h1 className="text-5xl font-semibold">Experience</h1>
+			<p>Add previous jobs related to the job you're applying to</p>
+			<button
+				onClick={handleAddJob}
+				className="bg-emphasis-500 font-semibold text-slate-500 py-2 px-5 hover:text-black duration-200"
+			>
+				Add Job
+			</button>
+			<div className="bg-slate-100 p-4 mr-5">
+				{userData.experience.map((exp) => {
+					console.log(exp);
+					return (
+						<Jobs
+							userData={userData}
+							setFormData={setFormData}
+							index={exp.id}
+							key={exp.id}
+						/>
+					);
+				})}
 			</div>
 		</AnimatedPage>
 	);
