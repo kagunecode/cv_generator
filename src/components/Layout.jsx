aa9deae22086bfcaed001a46a016d584c53c0489
kagunecode <cesar.acb98@gmail.com>
2023-12-12 08:50:14 -0500

    Removed data passing from the useState hook

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,11 @@
 import PreviewCV from "./PreviewCV";
 
-export default function Layout({ children, userData }) {
+export default function Layout({ children }) {
 	return (
 		<div className="grid grid-cols-3 w-[calc(100vw-3rem)] h-full translate-x-[3rem] p-3">
 			<div className="col-span-2">{children}</div>
 			<div className="bg-slate-200 p-2 col-span-1">
-				<PreviewCV formData={userData}></PreviewCV>
+				<PreviewCV></PreviewCV>
 			</div>
 		</div>
 	);
