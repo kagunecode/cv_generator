1d726eb88f3ed4c3018a88f907ae6de3378ed361
kagunecode <cesar.acb98@gmail.com>
2023-12-18 11:29:15 -0500

    Changed layout styling

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,7 +4,7 @@ export default function Layout({ children }) {
   return (
     <div className="grid h-full w-[calc(100vw-3rem)] translate-x-[3rem] grid-cols-3 p-3">
       <div className="col-span-2">{children}</div>
-      <div className="col-span-1 h-full bg-slate-200 p-2">
+      <div className="col-span-1 bg-zinc-200 p-2">
         <PreviewCV></PreviewCV>
       </div>
     </div>
