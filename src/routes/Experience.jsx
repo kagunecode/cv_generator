3396f5a9fdad5111a2b721392175e8903f6026b4
kagunecode <cesar.acb98@gmail.com>
2023-12-19 13:36:32 -0500

    Added animations for cards when collapsing

--- src/routes/Experience.jsx
+++ src/routes/Experience.jsx
@@ -60,14 +60,25 @@ function JobRender() {
   };
 
   const cardVariants = {
-    open: { opacity: 1, y: 0, display: 'grid' },
+    open: {
+      opacity: 1,
+      y: 0,
+      display: 'grid',
+      visibility: 'visible',
+      pointerEvents: 'auto',
+    },
     closed: {
       opacity: 0,
-      y: -10,
-      display: 'none',
+      y: -50,
       height: '0px',
-      zIndex: -20,
+      visibility: 'visible',
+      pointerEvents: 'none',
     },
+  };
+
+  const arrowVariants = {
+    open: { rotate: 0 },
+    closed: { rotate: 180 },
   };
 
   const jobs = data.experience.map(job => {
@@ -99,13 +110,16 @@ function JobRender() {
                 d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
               />
             </svg>
-            <svg
+            <motion.svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
               strokeWidth="1.5"
               stroke="currentColor"
               className="h-6 w-6 text-slate-500 duration-200 hover:cursor-pointer hover:text-black"
+              variants={arrowVariants}
+              animate={job.status ? 'open' : 'closed'}
+              transition={{ duration: 0.05 }}
               onClick={() => handleCardStatus(job.id, !job.status)}
             >
               <path
@@ -113,117 +127,119 @@ function JobRender() {
                 strokeLinejoin="round"
                 d="M12 19.5v-15m0 0l-6.75 6.75M12 4.5l6.75 6.75"
               />
-            </svg>
+            </motion.svg>
           </div>
         </div>
-        <motion.div
-          animate={job.status ? 'open' : 'closed'}
-          variants={cardVariants}
-          className="grid-cols-2"
-          key={job.id}
-        >
-          <div className="flex flex-col p-2">
-            <label htmlFor="">Company</label>
-            <input
-              type="text"
-              value={job.company}
-              className="h-8 w-full border border-zinc-300 px-1"
-              onChange={e =>
-                handleInputChange('company', e.target.value, job.id)
-              }
-            />
-          </div>
-          <div className="flex flex-col p-2">
-            <label htmlFor="">Position</label>
-            <input
-              type="text"
-              value={job.position}
-              className="h-8 w-full border border-zinc-300 px-1"
-              onChange={e =>
-                handleInputChange('position', e.target.value, job.id)
-              }
-            />
-          </div>
-          <div className="grid grid-cols-2">
+        <div className="overflow-hidden">
+          <motion.div
+            animate={job.status ? 'open' : 'closed'}
+            variants={cardVariants}
+            className="grid grid-cols-2"
+            key={job.id}
+          >
             <div className="flex flex-col p-2">
-              <label htmlFor="">From</label>
-              <div className="grid grid-cols-3 gap-2">
-                <select
-                  className="col-span-2 h-8 w-full border border-zinc-300 px-1"
-                  onChange={e =>
-                    handleInputChange('startDate', e.target.value, job.id)
-                  }
-                >
-                  {months.map(month => {
-                    return (
-                      <option key={month} value={`${month}`}>
-                        {month}
-                      </option>
-                    );
-                  })}
-                </select>
-                <select
-                  className="h-8 w-full border border-zinc-300 px-1"
-                  onChange={e =>
-                    handleInputChange('startYear', e.target.value, job.id)
-                  }
-                >
-                  {years.map(year => {
-                    return (
-                      <option key={year} value={`${year}`}>
-                        {year}
-                      </option>
-                    );
-                  })}
-                </select>
+              <label htmlFor="">Company</label>
+              <input
+                type="text"
+                value={job.company}
+                className="h-8 w-full border border-zinc-300 px-1"
+                onChange={e =>
+                  handleInputChange('company', e.target.value, job.id)
+                }
+              />
+            </div>
+            <div className="flex flex-col p-2">
+              <label htmlFor="">Position</label>
+              <input
+                type="text"
+                value={job.position}
+                className="h-8 w-full border border-zinc-300 px-1"
+                onChange={e =>
+                  handleInputChange('position', e.target.value, job.id)
+                }
+              />
+            </div>
+            <div className="grid grid-cols-2">
+              <div className="flex flex-col p-2">
+                <label htmlFor="">From</label>
+                <div className="grid grid-cols-3 gap-2">
+                  <select
+                    className="col-span-2 h-8 w-full border border-zinc-300 px-1"
+                    onChange={e =>
+                      handleInputChange('startDate', e.target.value, job.id)
+                    }
+                  >
+                    {months.map(month => {
+                      return (
+                        <option key={month} value={`${month}`}>
+                          {month}
+                        </option>
+                      );
+                    })}
+                  </select>
+                  <select
+                    className="h-8 w-full border border-zinc-300 px-1"
+                    onChange={e =>
+                      handleInputChange('startYear', e.target.value, job.id)
+                    }
+                  >
+                    {years.map(year => {
+                      return (
+                        <option key={year} value={`${year}`}>
+                          {year}
+                        </option>
+                      );
+                    })}
+                  </select>
+                </div>
+              </div>
+              <div className="flex flex-col p-2">
+                <label htmlFor="">To</label>
+                <div className="grid grid-cols-3 gap-2">
+                  <select
+                    className="col-span-2 h-8 w-full border border-zinc-300 px-1"
+                    onChange={e =>
+                      handleInputChange('endDate', e.target.value, job.id)
+                    }
+                  >
+                    {months.map(month => {
+                      return (
+                        <option key={month} value={`${month}`}>
+                          {month}
+                        </option>
+                      );
+                    })}
+                  </select>
+                  <select
+                    className="h-8 w-full border border-zinc-300 px-1"
+                    onChange={e =>
+                      handleInputChange('endYear', e.target.value, job.id)
+                    }
+                  >
+                    {years.map(year => {
+                      return (
+                        <option key={year} value={`${year}`}>
+                          {year}
+                        </option>
+                      );
+                    })}
+                  </select>
+                </div>
               </div>
             </div>
             <div className="flex flex-col p-2">
-              <label htmlFor="">To</label>
-              <div className="grid grid-cols-3 gap-2">
-                <select
-                  className="col-span-2 h-8 w-full border border-zinc-300 px-1"
-                  onChange={e =>
-                    handleInputChange('endDate', e.target.value, job.id)
-                  }
-                >
-                  {months.map(month => {
-                    return (
-                      <option key={month} value={`${month}`}>
-                        {month}
-                      </option>
-                    );
-                  })}
-                </select>
-                <select
-                  className="h-8 w-full border border-zinc-300 px-1"
-                  onChange={e =>
-                    handleInputChange('endYear', e.target.value, job.id)
-                  }
-                >
-                  {years.map(year => {
-                    return (
-                      <option key={year} value={`${year}`}>
-                        {year}
-                      </option>
-                    );
-                  })}
-                </select>
-              </div>
+              <label htmlFor="">Description</label>
+              <input
+                type="text"
+                value={job.description}
+                className="h-8 w-full border border-zinc-300 px-1"
+                onChange={e =>
+                  handleInputChange('description', e.target.value, job.id)
+                }
+              />
             </div>
-          </div>
-          <div className="flex flex-col p-2">
-            <label htmlFor="">Description</label>
-            <input
-              type="text"
-              value={job.description}
-              className="h-8 w-full border border-zinc-300 px-1"
-              onChange={e =>
-                handleInputChange('description', e.target.value, job.id)
-              }
-            />
-          </div>
-        </motion.div>
+          </motion.div>
+        </div>
       </motion.div>
     );
   });
