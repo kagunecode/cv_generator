9aa6b307a241ecd80f8babe82d60546a1327bb93
kagunecode <cesar.acb98@gmail.com>
2024-01-11 00:58:49 -0500

    Changed field arguments for general

--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -2,11 +2,7 @@ export default function Field({ name, field, set, index, ...props }) {
   return (
     <>
       <label htmlFor={field}>{name}</label>
-      <input
-        id={field}
-        onChange={e => set(field, e.target.value, index)}
-        {...props}
-      />
+      <input id={field} onChange={e => set(field, e.target.value)} {...props} />
     </>
   );
 }
