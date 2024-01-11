8fed4040b903fb059be8abe2751643d657eb206c
kagunecode <cesar.acb98@gmail.com>
2024-01-11 12:10:53 -0500

    Created update function for deeply nested fields

--- src/store.js
+++ src/store.js
@@ -3,9 +3,16 @@ import cvData from './data/cvData';
 
 const useData = create(set => ({
   generalInfo: cvData.generalInfo,
+  experience: cvData.experience,
   updateGeneral: (field, value) =>
     set(state => ({
       generalInfo: { ...state.generalInfo, [field]: value },
+    })),
+  updateItem: (section, index, field, value) =>
+    set(state => ({
+      [section]: state[section].map((item, i) =>
+        i === index ? { ...item, [field]: value } : item,
+      ),
     })),
 }));
 
