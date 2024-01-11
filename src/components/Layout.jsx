89c6316d794e9ab72083e1e432c6de15b0cb901d
kagunecode <cesar.acb98@gmail.com>
2024-01-11 11:02:17 -0500

    Added a div inside the sections to prevent scroll

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,7 +3,9 @@ import PreviewCV from './PreviewCV';
 export default function Layout({ children }) {
   return (
     <div className="grid h-full w-[calc(100vw-3rem)] translate-x-[3rem] grid-cols-3 p-3">
-      <div className="col-span-2">{children}</div>
+      <div className="col-span-2 overflow-y-auto">
+        <div>{children}</div>
+      </div>
       <div className="col-span-1 bg-zinc-200 p-2">
         <PreviewCV></PreviewCV>
       </div>
