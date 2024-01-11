ab7f9ddb3c06ed9eeefea5a3f748615bac55bf87
kagunecode <cesar.acb98@gmail.com>
2024-01-11 13:28:16 -0500

    Added item deletion and fixed item update

--- src/store.js
+++ src/store.js
@@ -4,16 +4,26 @@ import cvData from './data/cvData';
 const useData = create(set => ({
   generalInfo: cvData.generalInfo,
   experience: cvData.experience,
+
   updateGeneral: (field, value) =>
     set(state => ({
       generalInfo: { ...state.generalInfo, [field]: value },
     })),
+
   updateItem: (section, index, field, value) =>
     set(state => ({
-      [section]: state[section].map((item, i) =>
-        i === index ? { ...item, [field]: value } : item,
+      [section]: state[section].map(item =>
+        item.id === index ? { ...item, [field]: value } : item,
       ),
     })),
+
+  deleteItem: (section, id) =>
+    set(state => ({
+      [section]: state[section].filter(item => item.id !== id),
+    })),
+
+  addItem: (section, newItem) =>
+    set(state => ({ [section]: [...state[section], newItem] })),
 }));
 
 export { useData };
