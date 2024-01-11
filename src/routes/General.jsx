b4cc5dec8eee80ae1796a3bd876750f6b13d7d38
kagunecode <cesar.acb98@gmail.com>
2024-01-11 01:40:37 -0500

    Replaced all inputs for custom Field components

--- src/routes/General.jsx
+++ src/routes/General.jsx
@@ -3,9 +3,10 @@ import axios from 'axios';
 import { motion } from 'framer-motion';
 
 import ImageUpload from '../components/ImageUpload';
-import Field from '../components/Field';
+import { Field, CountryField } from '../components/Field';
 
 import { useDataContext } from '../contexts/DataContext';
+import { useData } from '../store';
 
 function General() {
   // TODO: Move variants to a separate file
@@ -31,37 +32,10 @@ function General() {
 }
 
 function GeneralForm() {
-  const { data, setData } = useDataContext();
-  const [countries, setCountries] = useState([]);
-
-  // TODO: Move the fetches into their own utility module for better management
-  useEffect(() => {
-    axios.get('https://restcountries.com/v3.1/all').then(response => {
-      const alphCountry = response.data.sort((a, b) => {
-        const nameA = a.name.common;
-        const nameB = b.name.common;
-
-        if (nameA < nameB) {
-          return -1;
-        }
-        if (nameA > nameB) {
-          return 1;
-        }
-        return 0;
-      });
-      setCountries(alphCountry);
-    });
-  }, []);
-
-  // TODO: Check if the input changes can be managed with a global setter instead
-  const handleInputChange = (fieldId, value, index) => {
-    setData(prevData => ({
-      ...prevData,
-      generalInfo: prevData['generalInfo'].map((item, i) =>
-        i === index ? { ...item, [fieldId]: value } : item,
-      ),
-    }));
-  };
+  const [generalInfo, updateGeneral] = useData(state => [
+    state.generalInfo,
+    state.updateGeneral,
+  ]);
 
   return (
     <div className="row-auto mr-5 mt-5 grid flex-1 grid-cols-1 border bg-zinc-50 p-5">
@@ -69,95 +43,77 @@ function GeneralForm() {
         <Field
           name="Full Name"
           field="fullname"
-          set={handleInputChange}
-          index={0}
+          set={updateGeneral}
           className="mr-4 h-8 border border-zinc-300 px-1"
-          value={data.generalInfo[0].fullname}
+          value={generalInfo.fullname}
         />
       </div>
       <div className="flex flex-col">
-        <label htmlFor="age">
-          Age
-          <span className="text-xs text-slate-600">
-            {' '}
-            (no specific formatting)
-          </span>
-        </label>
-        <input
-          id="age"
-          type="text"
+        <Field
+          name="Age"
+          field="age"
+          set={updateGeneral}
           className="mr-4 h-8 border border-zinc-300 px-1"
-          onChange={e => handleInputChange('age', e.target.value, 0)}
-          value={data.generalInfo[0].age}
+          value={generalInfo.age}
+          desc="(no specific formatting)"
+        ></Field>
+      </div>
+      <div className="flex flex-col">
+        <Field
+          name="Title"
+          field="title"
+          set={updateGeneral}
+          className="mr-4 h-8 border border-zinc-300 px-1"
+          value={generalInfo.title}
         />
       </div>
       <div className="flex flex-col">
-        <label htmlFor="title">Title</label>
-        <input
-          id="title"
-          type="text"
+        <Field
+          name="Email"
+          field="email"
+          set={updateGeneral}
           className="mr-4 h-8 border border-zinc-300 px-1"
-          onChange={e => handleInputChange('title', e.target.value, 0)}
-          value={data.generalInfo[0].title}
+          value={generalInfo.email}
         />
       </div>
       <div className="flex flex-col">
-        <label htmlFor="email">Email</label>
-        <input
-          id="email"
-          type="text"
+        <Field
+          name="Phone"
+          field="phone"
+          set={updateGeneral}
           className="mr-4 h-8 border border-zinc-300 px-1"
-          onChange={e => handleInputChange('email', e.target.value, 0)}
-          value={data.generalInfo[0].email}
+          value={generalInfo.phone}
         />
       </div>
       <div className="flex flex-col">
-        <label htmlFor="phone">Phone</label>
-        <input
-          id="phone"
-          type="text"
+        <CountryField
+          name="Country"
+          field="country"
+          set={updateGeneral}
           className="mr-4 h-8 border border-zinc-300 px-1"
-          onChange={e => handleInputChange('phone', e.target.value, 0)}
-          value={data.generalInfo[0].phone}
+          value={generalInfo.country}
         />
       </div>
       <div className="flex flex-col">
-        <label htmlFor="location">Country</label>
-        <select
-          id="location"
+        <Field
+          name="City"
+          field="city"
+          set={updateGeneral}
           className="mr-4 h-8 border border-zinc-300 px-1"
-          onChange={e => handleInputChange('country', e.target.value, 0)}
-          value={data.generalInfo[0].country}
-        >
-          {countries.map(country => {
-            return (
-              <option key={country.name.common} value={country.name.common}>
-                {country.name.common}
-              </option>
-            );
-          })}
-        </select>
-      </div>
-      <div className="flex flex-col">
-        <label htmlFor="location">City</label>
-        <input
-          id="location"
-          className="mr-4 h-8 border border-zinc-300 px-1"
-          onChange={e => handleInputChange('city', e.target.value, 0)}
-          value={data.generalInfo[0].city}
-        ></input>
+          value={generalInfo.city}
+        />
       </div>
       <div className="grid grid-cols-2">
         <div className="flex flex-col">
-          <label htmlFor="about">About you</label>
-          <textarea
-            id="about"
+          <Field
+            name="About You"
+            field="about"
+            set={updateGeneral}
             className="mr-4 h-40 border border-zinc-300 p-1"
-            onChange={e => handleInputChange('about', e.target.value, 0)}
-            value={data.generalInfo[0].about}
+            value={generalInfo.about}
+            textArea
           />
         </div>
-        <ImageUpload></ImageUpload>
       </div>
     </div>
   );
