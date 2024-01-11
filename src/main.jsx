2fdc9e75b76a0c6eb129b2bc0b141a4a9d35de7b
kagunecode <cesar.acb98@gmail.com>
2024-01-11 17:31:34 -0500

    Deleted DataContext for easier store usage

--- src/main.jsx
+++ src/main.jsx
@@ -13,22 +13,19 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // Styles
 import './styles.css';
 
-import DataContextProvider from './contexts/DataContext';
 import SidebarContextProvider from './contexts/SidebarContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <DataContextProvider>
-      <SidebarContextProvider>
-        <AnimatePresence mode="wait">
-          <Router>
-            <Routes>
-              <Route path="/error/*" element={<ErrorPage />} />
-              <Route path="*" element={<App />} />
-            </Routes>
-          </Router>
-        </AnimatePresence>
-      </SidebarContextProvider>
-    </DataContextProvider>
+    <SidebarContextProvider>
+      <AnimatePresence mode="wait">
+        <Router>
+          <Routes>
+            <Route path="/error/*" element={<ErrorPage />} />
+            <Route path="*" element={<App />} />
+          </Routes>
+        </Router>
+      </AnimatePresence>
+    </SidebarContextProvider>
   </React.StrictMode>,
 );
