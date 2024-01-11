7124ab37a99aeb78bde334f250f67eaa403bd438
kagunecode <cesar.acb98@gmail.com>
2024-01-11 16:19:50 -0500

    Removed x overflow when switching tabs

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,7 +3,7 @@ import PreviewCV from './PreviewCV';
 export default function Layout({ children }) {
   return (
     <div className="grid h-full w-[calc(100vw-3rem)] translate-x-[3rem] grid-cols-3 p-3">
-      <div className="col-span-2 overflow-y-auto">
+      <div className="col-span-2 overflow-y-auto overflow-x-hidden">
         <div>{children}</div>
       </div>
       <div className="col-span-1 bg-zinc-200 p-2">
