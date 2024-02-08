c7078f9bec9eb01f6407fd21caa3a7650db03f9d
kagunecode <cesar.acb98@gmail.com>
2024-02-08 12:09:57 -0500

    Changed initial welcome screen

--- src/routes/Welcome.jsx
+++ src/routes/Welcome.jsx
@@ -1,12 +1,20 @@
 import { AnimatedPage } from '../components/AnimatedPage';
+import { Link } from 'react-router-dom';
 
 function Welcome() {
   return (
     <>
       <AnimatedPage>
-        <div>
-          <h1 className="text-5xl font-semibold">Welcome</h1>
-          <p>Pick a section to start</p>
+        <div className="flex h-screen flex-col items-center justify-center">
+          <h1 className="text-[10rem] font-semibold tracking-tighter">
+            Welcome
+          </h1>
+          <Link
+            to="/general"
+            className="border px-16 py-4 text-[2rem] duration-200 hover:bg-emphasis-500 "
+          >
+            Start Now!
+          </Link>
         </div>
       </AnimatedPage>
     </>
