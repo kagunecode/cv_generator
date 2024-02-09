27f01b73176e32c9b091f298d5e4bdfc52ff6d1e
kagunecode <cesar.acb98@gmail.com>
2024-02-09 17:00:13 -0500

    Structured CV Preview for PDF generation

--- src/components/PreviewCV.jsx
+++ src/components/PreviewCV.jsx
@@ -1,82 +1,189 @@
+import React, { useState } from 'react';
 import { useData } from '../store';
+import {
+  PDFViewer,
+  Document,
+  Page,
+  Text,
+  View,
+  Image,
+  StyleSheet,
+  PDFDownloadLink,
+  Font,
+} from '@react-pdf/renderer';
+import cvData from '../data/cvData';
+import Inter from '/fonts/Inter.ttf';
+import InterBold from '/fonts/Inter-Bold.ttf';
+import InterItalic from '/fonts/Inter-Italic.ttf';
+
+Font.register({
+  family: 'Inter',
+  fonts: [
+    { src: Inter },
+    { src: InterBold, fontWeight: 700 },
+    { src: InterItalic, fontStyle: 'italic' },
+  ],
+});
+
+const styles = StyleSheet.create({
+  page: {
+    fontFamily: 'Inter',
+    padding: 20,
+    backgroundColor: '#f4f4f4',
+  },
+  header: {
+    backgroundColor: '#FFC107',
+    color: 'black',
+    padding: 20,
+    marginBottom: 20,
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'column',
+    borderRadius: '20px',
+  },
+  profileImage: {
+    borderRadius: '50%',
+    width: 100,
+    height: 100,
+    marginBottom: 10,
+  },
+  intro: {
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  section: {
+    marginBottom: 20,
+  },
+  subheader: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  text: {
+    fontSize: 12,
+    marginBottom: 8,
+  },
+  bold: {
+    fontWeight: 'bold',
+  },
+});
+
+function PDFDocument({ generalInfo, education, experience, skills }) {
+  return (
+    <Document>
+      <Page size="A4" style={styles.page}>
+        <View style={styles.header}>
+          {generalInfo.photo && (
+            <Image style={styles.profileImage} src={generalInfo.photo} />
+          )}
+          <Text style={{ fontSize: 24, fontWeight: 'bold', color: 'black' }}>
+            {generalInfo.fullname}
+          </Text>
+          <Text style={{ fontSize: 16, fontStyle: 'italic', color: 'black' }}>
+            {generalInfo.title}
+          </Text>
+          <Text
+            style={styles.text}
+          >{`${generalInfo.age}, ${generalInfo.country}, ${generalInfo.city}`}</Text>
+          <Text style={styles.text}>{generalInfo.email}</Text>
+          <Text style={styles.text}>{generalInfo.phone}</Text>
+          <Text style={styles.intro}>{generalInfo.about}</Text>
+        </View>
+
+        <View style={styles.section}>
+          <Text style={styles.subheader}>Experience</Text>
+          {experience.map(job => (
+            <View key={job.id} style={styles.text}>
+              <Text style={styles.bold}>{job.position}</Text>
+              <Text>{job.company}</Text>
+              <Text>{`${job.startDate} ${job.startYear} - ${job.endDate} ${job.endYear}`}</Text>
+              <Text>{job.description}</Text>
+            </View>
+          ))}
+        </View>
+
+        <View style={styles.section}>
+          <Text style={styles.subheader}>Education</Text>
+          {education.map(ed => (
+            <View key={ed.id} style={styles.text}>
+              <Text style={styles.bold}>{ed.title}</Text>
+              <Text>{ed.institution}</Text>
+              <Text>{`${ed.start} - ${ed.end}`}</Text>
+              <Text>{ed.degree}</Text>
+            </View>
+          ))}
+        </View>
+
+        <View style={styles.section}>
+          <Text style={styles.subheader}>Skills</Text>
+          {skills.map(skill => (
+            <View key={skill.id} style={styles.text}>
+              <Text style={styles.bold}>{skill.name}</Text>
+              <Text>{skill.expertise}</Text>
+            </View>
+          ))}
+        </View>
+      </Page>
+    </Document>
+  );
+}
 
 function PreviewCV() {
-  const [generalInfo, experience, education, skills] = useData(state => [
-    state.generalInfo,
-    state.experience,
-    state.education,
-    state.skills,
-  ]);
+  const [generalInfo, setGeneralInfo] = useState(cvData.generalInfo);
+  const [experience, setExperience] = useState(cvData.experience);
+  const [education, setEducation] = useState(cvData.education);
+  const [skills, setSkills] = useState(cvData.skills);
+
+  const [dataGeneralInfo, dataExperience, dataEducation, dataSkills] = useData(
+    state => [
+      state.generalInfo,
+      state.experience,
+      state.education,
+      state.skills,
+    ],
+  );
+
+  function handleRender() {
+    setGeneralInfo(dataGeneralInfo);
+    setExperience(dataExperience);
+    setEducation(dataEducation);
+    setSkills(dataSkills);
+  }
 
   return (
-    <div className="h-full bg-white p-6">
-      <div className="h-30 mb-6 rounded-lg bg-emphasis-500 p-5">
-        <div className="mb-4 flex flex-col items-center">
-          {generalInfo.photo && (
-            <img
-              className="mb-2 h-24 w-24 rounded-full object-cover"
-              src={generalInfo.photo}
-              alt=""
-            />
-          )}
-          <h1 className="text-2xl font-bold">{generalInfo.fullname}</h1>
-          <p className="text-sm font-light">{generalInfo.title}</p>
-        </div>
-        <div className="mb-2">
-          <p className="text-sm font-light">
-            {generalInfo.age}, {generalInfo.country}, {generalInfo.city}
-          </p>
-          <p className="text-sm font-light">{generalInfo.email}</p>
-          <p className="text-sm font-light">{generalInfo.phone}</p>
-        </div>
-        <p className="text-sm font-light">{generalInfo.about}</p>
-      </div>
-
-      <div className="mb-6">
-        <div className="grid grid-cols-2 gap-6">
-          <div>
-            <h1 className="mb-2 text-lg font-bold">Experience</h1>
-            {experience.map(job => (
-              <div key={job.id} className="mb-4">
-                <p className="text-sm font-bold">{job.company}</p>
-                <p className="text-sm font-light">{job.position}</p>
-                <div className="flex gap-2 text-xs">
-                  <p>{`${job.startDate} ${job.startYear}`}</p>
-                  {job.startDate && <p className="font-bold">-</p>}
-                  <p>{`${job.endDate} ${job.endYear}`}</p>
-                </div>
-                <p className="text-sm font-light">{job.description}</p>
-              </div>
-            ))}
-          </div>
-
-          <div>
-            <h1 className="mb-2 text-lg font-bold">Education</h1>
-            {education.map(ed => (
-              <div key={ed.id} className="mb-4">
-                <p className="text-sm font-bold">{ed.institution}</p>
-                <p className="text-sm font-light">{ed.title}</p>
-                <div className="flex gap-2 text-xs">
-                  <p>{`${ed.start}`}</p>
-                  {ed.start && <p className="font-bold">-</p>}
-                  <p>{`${ed.end}`}</p>
-                </div>
-                <p className="text-sm font-light">{ed.degree}</p>
-              </div>
-            ))}
-          </div>
+    <div className="flex h-full w-full flex-col">
+      <div className="mb-2 flex items-center justify-between gap-10">
+        <button
+          className="border bg-gray-50 px-10 py-2 font-bold duration-200 hover:bg-emphasis-500"
+          onClick={handleRender}
+        >
+          Compile
+        </button>
+        <div className="border bg-gray-50 px-10 py-2 font-bold duration-200 hover:bg-emphasis-500">
+          <PDFDownloadLink
+            document={
+              <PDFDocument
+                generalInfo={generalInfo}
+                experience={experience}
+                education={education}
+                skills={skills}
+              />
+            }
+          >
+            {({ blob, url, loading, error }) =>
+              loading ? 'Loading...' : 'Download'
+            }
+          </PDFDownloadLink>
         </div>
       </div>
-
-      <div>
-        <h1 className="mb-2 text-lg font-bold">Skills</h1>
-        {skills.map(skill => (
-          <div key={skill.id} className="mb-3">
-            <p className="text-sm font-bold">{skill.name}</p>
-            <p className="text-xs font-light">{skill.expertise}</p>
-          </div>
-        ))}
-      </div>
+      <PDFViewer showToolbar={false} className="h-full w-full">
+        <PDFDocument
+          generalInfo={generalInfo}
+          experience={experience}
+          education={education}
+          skills={skills}
+        />
+      </PDFViewer>
     </div>
   );
 }
