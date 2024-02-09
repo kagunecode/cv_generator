70332de76c329e97aed25509aa17093d878c607e
kagunecode <cesar.acb98@gmail.com>
2024-02-09 18:04:32 -0500

    Added information on a simple welcome screen

--- src/routes/Welcome.jsx
+++ src/routes/Welcome.jsx
@@ -5,13 +5,18 @@ function Welcome() {
   return (
     <>
       <AnimatedPage>
-        <div className="flex h-screen flex-col items-center justify-center">
-          <h1 className="text-[10rem] font-semibold tracking-tighter">
-            Welcome
+        <div className="absolute left-0 top-0 flex h-full w-full flex-col items-center justify-center">
+          <h1 className="text-[7rem] font-semibold tracking-tighter">
+            Kagune CV Generator
           </h1>
+          <p>
+            This CV generator is still in beta, more functions will be added in
+            the future. Please, refer to my social media for suggestions or
+            requests.
+          </p>
           <Link
             to="/general"
-            className="border px-16 py-4 text-[2rem] duration-200 hover:bg-emphasis-500 "
+            className="mt-5 bg-gray-50 px-5 py-3 text-[1.5rem] font-bold duration-200 hover:bg-emphasis-500 "
           >
             Start Now!
           </Link>
