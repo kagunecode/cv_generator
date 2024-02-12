ba91aa088d7c88df94461b6a5172e80870f4d1cf
kagunecode <cesar.acb98@gmail.com>
2024-02-11 21:07:15 -0500

    Added sidebar variants

--- src/variants.js
+++ src/variants.js
@@ -25,4 +25,34 @@ const arrowVariants = {
   closed: { rotate: 180 },
 };
 
-export { cardAnimations, cardVariants, arrowVariants };
+const menuSlideSidebar = {
+  initial: { x: 'calc(-100% - 100px)' },
+  enter: {
+    x: '0',
+    transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1] },
+  },
+  exit: {
+    x: 'calc(-100% - 100px)',
+    transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1] },
+  },
+};
+
+const linkSlideSidebar = {
+  initial: { x: '-80px' },
+  enter: i => ({
+    x: '0px',
+    transition: { duration: 0.8, ease: [0.45, 0, 0.55, 1], delay: 0.03 * i },
+  }),
+  exit: i => ({
+    x: '-80px',
+    transition: { duration: 0.5, ease: [0.45, 0, 0.55, 1], delay: 0.03 * i },
+  }),
+};
+
+export {
+  cardAnimations,
+  cardVariants,
+  arrowVariants,
+  menuSlideSidebar,
+  linkSlideSidebar,
+};
