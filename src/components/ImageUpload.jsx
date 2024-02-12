b0be64d0a3f67db2d5dc684c59037b3fc53e723f
kagunecode <cesar.acb98@gmail.com>
2024-02-11 20:40:38 -0500

    Changed image upload layout and function

--- src/components/ImageUpload.jsx
+++ src/components/ImageUpload.jsx
@@ -1,28 +1,30 @@
 import { useData } from '../store';
+import imageUpload from '../utilities/imageUpload';
 
 export default function ImageUpload() {
-  const updateItem = useData(state => state.updateItem);
-
-  const handleImageUpload = img => {
-    const imageUrl = URL.createObjectURL(img);
-    updateItem('generalInfo', 0, 'photo', imageUrl);
-  };
+  const [generalInfo, updateItem] = useData(state => [
+    state.generalInfo,
+    state.updateItem,
+  ]);
 
   return (
-    <div className="mb-4 flex h-[50%] justify-center self-center align-middle">
-      <label
-        className="mb-2 cursor-pointer self-center border px-20 py-8 font-bold text-slate-500 duration-150 hover:bg-emphasis-500 hover:text-black"
-        htmlFor="image_upload"
-      >
-        Upload Image
-      </label>
-      <input
-        id="image_upload"
-        type="file"
-        className="hidden"
-        onChange={e => handleImageUpload(e.target.files[0])}
-        accept="image/png, image/jpg, image/jpeg"
-      />
+    <div className="flex flex-col md:ml-4">
+      <h1>Photo</h1>
+      <div className="relative flex items-center justify-center">
+        <label className="left-50 bottom-50 absolute flex h-[15rem] w-[15rem] cursor-pointer items-center justify-center border text-2xl font-bold text-white opacity-5 duration-200 hover:opacity-100 hover:backdrop-blur-xl">
+          <h1>Upload Photo</h1>
+          <input
+            type="file"
+            className="hidden"
+            onChange={e => imageUpload(e.target.files[0], updateItem)}
+            accept="image/png, image/jpg, image/jpeg"
+          />
+        </label>
+        <img
+          src={generalInfo[0].photo}
+          className="col-span-2 h-[15rem] w-[15rem] justify-self-center border object-cover"
+        />
+      </div>
     </div>
   );
 }
