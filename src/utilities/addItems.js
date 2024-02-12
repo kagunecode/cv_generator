cceebf87988c7be6c9529e1526a0f0f5bbd39d4e
kagunecode <cesar.acb98@gmail.com>
2024-02-11 22:40:20 -0500

    Added addJob and addSkill functions

--- src/utilities/addItems.js
+++ src/utilities/addItems.js
@@ -15,4 +15,26 @@ function addEducation(addItem) {
   addItem('education', newEd);
 }
 
-export { addEducation };
+function addJob(addItem) {
+  const newJob = {
+    id: uuidv4(),
+    position: '',
+    company: '',
+    startDate: 'January',
+    startYear: '2021',
+    endDate: 'January',
+    endYear: '2024',
+    description: '',
+    status: true,
+  };
+  addItem('experience', newJob);
+}
+
+function addSkill(skillName, active, addItem, setSkillName) {
+  if (skillName == '') return;
+  const newItem = { id: uuidv4(), name: skillName, expertise: active };
+  addItem('skills', newItem);
+  setSkillName('');
+}
+
+export { addEducation, addJob, addSkill };
